c4473d259d5366ba640218c8320339bff3d268e6
Gustavo Schneiter <gmhschn@gmail.com>
2025-09-24 06:29:11 -0300

    chore(amm): amm_bail! sem ';' interno + macro_export dedup

--- src/amm/error.rs
+++ src/amm/error.rs
@@ -187,14 +187,9 @@ macro_rules! amm_err {
 }
 
 #[macro_export]
-#[macro_export]
-#[macro_export]
 macro_rules! amm_bail {
-($($tt:tt)*) => {{
-return Err($crate::amm_err!($($tt)*))
-}};
+($($tt:tt)*) => { return Err($crate::amm_err!($($tt)*)) };
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
