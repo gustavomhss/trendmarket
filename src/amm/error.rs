8c0bf066d395f8a0b31ad2934568d338022aff91
Gustavo Schneiter <gmhschn@gmail.com>
2025-09-24 03:48:46 -0300

    chore(amm): amm_bail! sem ';' no expansion (silencia warning de macro em posição de expressão)

--- src/amm/error.rs
+++ src/amm/error.rs
@@ -187,10 +187,7 @@ macro_rules! amm_err {
 }
 
 #[macro_export]
-macro_rules! amm_bail {
-  ($($tt:tt)*) => {
-    return Err($crate::amm_err!($($tt)*));
-  };
+macro_rules! amm_bail { ($($tt:tt)*) => {{ return Err($crate::amm_err!($($tt)*)) }}; };
 }
 
 #[cfg(test)]
