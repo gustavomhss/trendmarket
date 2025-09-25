33b97b411da6212c5f3042b9f770c3d0b68504fd
Gustavo Schneiter <gmhschn@gmail.com>
2025-09-25 16:44:35 -0300

    WIP: snapshot antes do patch unico (obs dup + PROM_REGISTRY)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 #[cfg(feature = "obs")]
 pub mod obs;
-#[cfg(feature = "obs")]
-pub mod obs;
 /* lib (CRD-7-10 FINAL) */
 pub mod amm; // existe
 pub mod ce_core; // expõe o namespace ce_core
-#[cfg(feature = "obs")]
-pub mod obs;
 
 pub mod telemetry;
