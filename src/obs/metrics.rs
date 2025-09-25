ab535e68a2eed6010b2c36e80023407df0dcf190
Gustavo Schneiter <gmhschn@gmail.com>
2025-09-25 06:12:53 -0300

    WIP: snapshot antes do hotfix obs/metrics

--- src/obs/metrics.rs
+++ src/obs/metrics.rs
@@ -1,5 +1,5 @@
-use prometheus::Encoder;
 #![cfg(feature = "obs")]
+use prometheus::Encoder;
 use prometheus::Encoder; // traz o trait p/ TextEncoder::encode
 
 pub fn start_metrics_http(addr: &str) {
